5a5f9d767a5bf649f42e5f552b186c0c4cf76caa
contiguima <contiguima@gmail.com>
2023-08-10 19:17:41 -0300

    [FIX] Problema de metadata en componente tipo client

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,8 @@
 "use client"
 import './globals.css'
-import { Inter } from 'next/font/google'
 import TagManager, {TagManagerArgs} from 'react-gtm-module';
 import { useEffect } from 'react';
-const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
-  title: 'Oxygen Token',
-  description: 'Landing Page de Oxygen Token',
-}
 
 export default function RootLayout({ children }) {
   const gtmId = process.env.NEXT_PUBLIC_GTM_ID || "";
