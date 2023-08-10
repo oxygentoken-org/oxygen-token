e838abeb4acd15537409ce4e0f3bcb3423ee2963
contiguima <contiguima@gmail.com>
2023-08-10 19:19:48 -0300

    [FIX] Problema de exportacion de fuente

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,9 @@
 "use client"
 import './globals.css'
+import { Inter } from 'next/font/google'
 import TagManager, {TagManagerArgs} from 'react-gtm-module';
 import { useEffect } from 'react';
+const inter = Inter({ subsets: ['latin'] })
 
 
 export default function RootLayout({ children }) {
